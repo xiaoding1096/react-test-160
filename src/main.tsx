73434090ec4d5decd475292aa8fbb839009a02ce
xiaoding1096 <hoangnguyendt1096@gmail.com>
2025-06-30 18:01:14 +0200

    installed 1-60 needed packed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Layout from './layout.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from '@/layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <div></div>,
+      },
+      {
+        path: "/",
+        element: <div></div>,
+      },
+      {
+        path: "/",
+        element: <div></div>,
+      },
+    ]
+  },
+]);
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
+import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/server-options.html#server-options
 dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000,
   },
